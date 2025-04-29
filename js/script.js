b2f3d6e7afa56b8c8bb941b85b2978b1b2d22b93
soyun9167 <soyun9167@gmail.com>
2025-04-29 09:32:33 +0900

    general 중간커밋

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const tabCtrl = (e, idx) => {
+    const tabParent = e.parentElement.parentElement, tabLi = tabParent.querySelectorAll('.nav_li'),
+        tabContent = tabParent.querySelectorAll('.tab_content');
+    for (const i of tabLi) i.classList.remove('active');
+    for (const i of tabContent) i.classList.remove('active');
+
+    tabLi[idx].classList.add('active');
+    tabContent[idx].classList.add('active');
+}
